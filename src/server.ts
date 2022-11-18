895795ca095097cbf7ed0ba8a5956c79f3d70142
Aluno IFAL <vmsvitor20@gmail.com>
2022-11-17 21:18:20 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import * as bodyParser from "body-parser";
+import express from 'express';
+import * as bodyParser from 'body-parser';
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', routerController);
 
 app.listen(3000, () => {
-    console.clear();
-    console.log('Server started in localhost:3000🚀');
+  console.clear();
+  console.log('Server started in localhost:3000🚀');
 });
