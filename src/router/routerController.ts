1b99f775814b06dd684e23839fb6a130b11125ce
vitormelods <vmsvitor20@gmail.com>
2022-11-21 21:29:04 -0300

    feat: add route of tracking

--- src/router/routerController.ts
+++ src/router/routerController.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
+import { ChargeTracking } from '../http/controllers/trackingController';
 
 const route: Router = Router();
+const charge: ChargeTracking = new ChargeTracking();
 
-route.get('/teste', (req: Request, res: Response) => {
-  res.status(200).send({message: 'oi'});
+route.get('/tracking', async (req: Request, res: Response) => {
+  await charge.getCharge(req, res);
 });
 
 export const routerController: Router = route;
