dd1fdd3aebaeee6d9fa8efa8bf0e108298badbf8
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-11-22 23:36:45 -0300

    feat: add environment variable for database

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,3 +4,9 @@ export const server = {
   port: process.env.APP_PORT,
   host: process.env.APP_HOST
 };
+
+export const db = {
+  user: process.env.BD_USER,
+  password: process.env.BD_PASS,
+  tns: process.env.DB_TNS
+};
