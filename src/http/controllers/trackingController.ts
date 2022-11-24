30c0db8d2ee319dc30440d8106469232e7348710
Aluno IFAL <vmsvitor20@gmail.com>
2022-11-23 21:13:54 -0300

    feat: add method for create charge

--- src/http/controllers/trackingController.ts
+++ src/http/controllers/trackingController.ts
@@ -18,4 +18,20 @@ export class ChargeTracking {
 
     return erro ? res.status(500).send(erro) : res.status(200).send({message: 'chegou aqui'});
   }
+
+  public async createCharge(req: Request, res: Response) {
+    const conn = DBconnection.conn();
+    let erro = '';
+
+    try {
+      await conn.table('tb_carga').insert({
+
+      });
+    } catch (e: any) {
+      console.log(e);
+      erro = e.message;
+    }
+
+    return erro ? res.status(500).send({ message: erro }) : res.status(201).send({ message: 'Carga cadastrada com sucesso!' });
+  }
 }
