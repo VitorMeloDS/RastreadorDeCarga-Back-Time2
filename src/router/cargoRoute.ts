c434f6efe1023cddb8ee389ced2a5deb82b9d66d
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-11-26 10:50:49 -0300

    feat: add cargoRoute

---
+++ src/router/cargoRoute.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express';
+import { Cargo } from '../http/controllers/cargoController';
+
+const route: Router = Router();
+const charge: Cargo = new Cargo();
+
+route.get('/cargo', async (req: Request, res: Response) => {
+  await charge.getCharge(req, res);
+});
+
+route.post('/cargo', async (req: Request, res: Response) => {
+  await charge.createCharge(req, res);
+});
+
+export const cargoRoute: Router = route;
--- src/router/routerController.ts
+++ src/router/routerController.ts
@@ -1,15 +1,9 @@
-import { Router, Request, Response } from 'express';
-import { Cargo } from '../http/controllers/cargoController';
+import { Router } from 'express';
+import { cargoRoute } from './cargoRoute';
 
 const route: Router = Router();
-const charge: Cargo = new Cargo();
 
-route.get('/cargo', async (req: Request, res: Response) => {
-  await charge.getCharge(req, res);
-});
-
-route.post('/cargo', async (req: Request, res: Response) => {
-  await charge.createCharge(req, res);
-});
+// * Cargo route
+route.use('/cargo', cargoRoute);
 
 export const routerController: Router = route;
