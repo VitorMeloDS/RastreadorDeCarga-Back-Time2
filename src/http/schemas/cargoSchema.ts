4ad30412c24265e80441c60c77ff705afa7b9af5
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-12-14 23:46:16 -0300

    feat: validator of createCargo

--- src/http/schemas/cargoSchema.ts
+++ src/http/schemas/cargoSchema.ts
@@ -3,5 +3,6 @@ import Joi from 'joi';
 export const cargoValidator = Joi.object({
   origem: Joi.string().required(),
   destino: Joi.string().required(),
+  status: Joi.string().required(),
   data_entrega: Joi.string().required()
 });
